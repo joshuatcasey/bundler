503ea6b127ece3fa0d6706b90fd79b95ebf5412e
Joshua Casey <joshuatcasey@gmail.com>
2022-06-08 13:59:01 -0500

    add retrieval step

--- dependency/retrieval/retrieve.go
+++ dependency/retrieval/retrieve.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"path/filepath"
+	"sort"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/paketo-buildpacks/packit/v2/cargo"
+	"golang.org/x/exp/slices"
 )
 
 type BundlerRelease struct {
@@ -12,6 +18,93 @@ type BundlerRelease struct {
 }
 
 func main() {
+	config := parseBuildpackToml()
+
+	buildpackVersions := getBuildpackVersions(config)
+	rubyGemVersions := getRubyGemVersions()
+	versionsFilteredByConstraints := filterToConstraints(config, rubyGemVersions)
+	versionsFilteredByPatches := filterToPatches(versionsFilteredByConstraints, config, buildpackVersions)
+	//bytes, err := json.Marshal(versionsFilteredByPatches)
+	//if err != nil {
+	//	return
+	//}
+	//fmt.Println(string(bytes))
+	if len(versionsFilteredByPatches) > 0 {
+		fmt.Println(versionsFilteredByPatches[0])
+	}
+}
+
+func filterToPatches(versionsFilteredByConstraints map[string][]*semver.Version, config cargo.Config, buildpackVersions []string) []string {
+	var versionsToAdd []*semver.Version
+	for constraintId, versions := range versionsFilteredByConstraints {
+		var buildpackConstraint cargo.ConfigMetadataDependencyConstraint
+		for _, constraint := range config.Metadata.DependencyConstraints {
+			if constraint.ID == constraintId {
+				buildpackConstraint = constraint
+			}
+		}
+
+		sort.Slice(versions, func(i, j int) bool {
+			return versions[i].LessThan(versions[j])
+		})
+
+		// if there are more requested patches than matching dependencies, just
+		// return all matching dependencies.
+		if buildpackConstraint.Patches > len(versions) {
+			continue
+		}
+
+		// Buildpack.toml dependencies are usually in order from lowest to highest
+		// version. We want to return the the n largest matching dependencies in the
+		// same order, n being the constraint.Patches field from the buildpack.toml.
+		// Here, we are returning the n highest matching Dependencies.
+		versionsToAdd = append(versionsToAdd, versions[len(versions)-buildpackConstraint.Patches:]...)
+	}
+
+	var versionsAsStrings []string
+	for _, version := range versionsToAdd {
+		versionAsString := version.String()
+		if !slices.Contains(buildpackVersions, versionAsString) {
+			versionsAsStrings = append(versionsAsStrings, versionAsString)
+		}
+	}
+
+	return versionsAsStrings
+}
+
+func filterToConstraints(config cargo.Config, rubyGemVersions []*semver.Version) map[string][]*semver.Version {
+	semverConstraints := make(map[string]*semver.Constraints)
+	for _, constraint := range config.Metadata.DependencyConstraints {
+		c, err := semver.NewConstraint(constraint.Constraint)
+		if err != nil {
+			panic(err)
+		}
+		semverConstraints[constraint.ID] = c
+	}
+
+	newVersions := make(map[string][]*semver.Version)
+	for _, version := range rubyGemVersions {
+		for constraintId, constraint := range semverConstraints {
+			if constraint.Check(version) {
+				newVersions[constraintId] = append(newVersions[constraintId], version)
+			}
+		}
+	}
+	return newVersions
+}
+
+func parseBuildpackToml() cargo.Config {
+	buildpackTomlPath := filepath.Join("..", "..", "buildpack.toml")
+
+	configParser := cargo.NewBuildpackParser()
+	config, err := configParser.Parse(buildpackTomlPath)
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse %s: %s", buildpackTomlPath, err))
+	}
+	return config
+}
+
+func getRubyGemVersions() []*semver.Version {
 	resp, err := http.Get("https://rubygems.org/api/v1/versions/bundler.json")
 	if err != nil {
 		panic("could not get release json")
@@ -29,10 +122,22 @@ func main() {
 		panic("could not unmarshal response")
 	}
 
-	var versions []string
+	var rubyGemVersions []*semver.Version
 	for _, br := range bundlerReleases {
-		versions = append(versions, br.Version)
+		version, err := semver.NewVersion(br.Version)
+		if err != nil {
+			continue
+		}
+		rubyGemVersions = append(rubyGemVersions, version)
 	}
 
-	fmt.Println(versions)
+	return rubyGemVersions
+}
+
+func getBuildpackVersions(config cargo.Config) []string {
+	var buildpackVersions []string
+	for _, d := range config.Metadata.Dependencies {
+		buildpackVersions = append(buildpackVersions, d.Version)
+	}
+	return buildpackVersions
 }
