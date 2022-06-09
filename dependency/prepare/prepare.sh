ece4d6dfd61db8c5c36cc48b4fa70e97651f96a4
Joshua Casey <joshuatcasey@gmail.com>
2022-06-08 21:31:37 -0500

    Add prepare step instead of inline brew install coreutils

---
+++ dependency/prepare/prepare.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+function main() {
+  local os isMacOs
+  os="${1}"
+
+  echo "${os}" | grep --quiet "macos"
+  isMacOs=$?
+
+  if [[ $isMacOs -eq 0 ]]; then
+    brew install coreutils
+  fi
+}
+
+main "${@:-}"
