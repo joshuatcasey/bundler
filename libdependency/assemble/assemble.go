b567b6936e12a966b8e082d090d37cd8d5c5e4de
Joshua Casey <joshuatcasey@gmail.com>
2022-06-14 11:53:57 -0500

    prepare commit

--- libdependency/assemble/assemble.go
+++ libdependency/assemble/assemble.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/joshuatcasey/bundler/libdependency/common"
+	"github.com/paketo-buildpacks/packit/v2/cargo"
 	"github.com/paketo-buildpacks/packit/v2/fs"
+	"golang.org/x/exp/slices"
 )
 
 type Artifact struct {
@@ -23,9 +27,11 @@ type Artifact struct {
 func main() {
 	id := os.Args[1]
 	artifactPath := os.Args[2]
+	buildpackTomlPath := os.Args[3]
 
 	fmt.Printf("id=%s\n", id)
 	fmt.Printf("artifactPath=%s\n", artifactPath)
+	fmt.Printf("buildpackTomlPath=%s\n", buildpackTomlPath)
 
 	if exists, err := fs.Exists(artifactPath); err != nil {
 		panic(err)
@@ -48,13 +54,31 @@ func main() {
 	fmt.Println("Found artifacts:")
 	printAsJson(artifacts)
 
-	prepareCommit(artifacts)
+	prepareCommit(artifacts, buildpackTomlPath)
+	prune(buildpackTomlPath)
 }
 
-func prepareCommit(artifacts []Artifact) {
-	buildpackTomlPath := filepath.Join("..", "..", "buildpack.toml")
+func prepareCommit(artifacts []Artifact, buildpackTomlPath string) {
 	config := common.ParseBuildpackToml(buildpackTomlPath)
-	
+
+	for _, artifact := range artifacts {
+		dependency := artifact.Metadata.ConfigMetadataDependency
+		dependency.URI = artifact.Uri
+		dependency.SHA256 = artifact.TarballSHA256
+		dependency.Stacks = append(dependency.Stacks, artifact.Os)
+		config.Metadata.Dependencies = append(config.Metadata.Dependencies, dependency)
+	}
+
+	file, err := os.OpenFile(buildpackTomlPath, os.O_RDWR|os.O_TRUNC, 0600)
+	if err != nil {
+		panic(fmt.Errorf("failed to open buildpack config file: %w", err))
+	}
+	defer file.Close()
+
+	err = cargo.EncodeConfig(file, config)
+	if err != nil {
+		panic(fmt.Errorf("failed to write buildpack config: %w", err))
+	}
 }
 
 func getMetadata(artifactPath string) map[string]*common.DepVersion {
@@ -179,4 +203,106 @@ func isOS(file os.FileInfo) bool {
 
 func isVersion(file os.FileInfo) bool {
 	return strings.HasSuffix(file.Name(), ".version")
+}
+
+func prune(buildpackTomlPath string) {
+	config := common.ParseBuildpackToml(buildpackTomlPath)
+
+	// Get a map from constraints to dependencies
+	constraintToDependencies := make(map[cargo.ConfigMetadataDependencyConstraint][]cargo.ConfigMetadataDependency)
+
+	for _, dependency := range config.Metadata.Dependencies {
+		dependencyVersionAsSemver := semver.MustParse(dependency.Version)
+		for _, constraint := range config.Metadata.DependencyConstraints {
+			constraintAsSemver, err := semver.NewConstraint(constraint.Constraint)
+			if err != nil {
+				panic(err)
+			}
+
+			if dependency.ID == constraint.ID && constraintAsSemver.Check(dependencyVersionAsSemver) {
+				constraintToDependencies[constraint] = append(constraintToDependencies[constraint], dependency)
+			}
+		}
+	}
+
+	constraintToPatches := make(map[cargo.ConfigMetadataDependencyConstraint][]string)
+
+	// We can have more than one dependency with the same version
+	// so we have to figure out which versions are captured in the patches
+	for constraint, dependencies := range constraintToDependencies {
+		for _, dependency := range dependencies {
+			if !slices.Contains(constraintToPatches[constraint], dependency.Version) {
+				constraintToPatches[constraint] = append(constraintToPatches[constraint], dependency.Version)
+			}
+		}
+
+		sort.Slice(constraintToPatches[constraint], func(i, j int) bool {
+			iVersion := semver.MustParse(constraintToPatches[constraint][i])
+			jVersion := semver.MustParse(constraintToPatches[constraint][j])
+			return iVersion.LessThan(jVersion)
+		})
+
+		if constraint.Patches < len(constraintToPatches[constraint]) {
+			constraintToPatches[constraint] = constraintToPatches[constraint][len(constraintToPatches[constraint])-constraint.Patches:]
+		}
+	}
+
+	var patchesToKeep []string
+	for _, versions := range constraintToPatches {
+		patchesToKeep = append(patchesToKeep, versions...)
+	}
+
+	fmt.Println("patchesToKeep")
+	fmt.Println(patchesToKeep)
+
+	var dependenciesToKeep []cargo.ConfigMetadataDependency
+
+	for _, dependency := range config.Metadata.Dependencies {
+		if slices.Contains(patchesToKeep, dependency.Version) {
+			dependenciesToKeep = append(dependenciesToKeep, dependency)
+		}
+	}
+
+	// Sort the stacks within the dependency
+	for _, dependency := range dependenciesToKeep {
+		sort.Slice(dependency.Stacks, func(i, j int) bool {
+			return dependency.Stacks[i] > dependency.Stacks[j]
+		})
+	}
+
+	// Sort the dependencies by:
+	// 1. ID
+	// 2. Version
+	// 3. len(Stacks)
+	sort.Slice(dependenciesToKeep, func(i, j int) bool {
+		dep1 := dependenciesToKeep[i]
+		dep2 := dependenciesToKeep[j]
+		if dep1.ID == dep2.ID {
+			dep1Version := semver.MustParse(dep1.Version)
+			dep2Version := semver.MustParse(dep2.Version)
+
+			if dep1Version.Equal(dep2Version) {
+				return len(dep1.Stacks) > len(dep2.Stacks)
+			}
+
+			return dep1Version.GreaterThan(dep2Version)
+		}
+		return dep1.ID > dep2.ID
+	})
+
+	fmt.Println("dependenciesToKeep")
+	fmt.Println(dependenciesToKeep)
+
+	config.Metadata.Dependencies = dependenciesToKeep
+
+	file, err := os.OpenFile(buildpackTomlPath, os.O_RDWR|os.O_TRUNC, 0600)
+	if err != nil {
+		panic(fmt.Errorf("failed to open buildpack config file: %w", err))
+	}
+	defer file.Close()
+
+	err = cargo.EncodeConfig(file, config)
+	if err != nil {
+		panic(fmt.Errorf("failed to write buildpack config: %w", err))
+	}
 }
--- libdependency/assemble/go.mod
+++ libdependency/assemble/go.mod
@@ -3,12 +3,9 @@ module github.com/joshuatcasey/bundler/retrieval
 go 1.18
 
 require (
-	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/joshuatcasey/bundler/libdependency/common v0.0.0-20220614162819-e3d9baac0990
 	github.com/paketo-buildpacks/packit/v2 v2.3.0
-	golang.org/x/exp v0.0.0-20220608143224-64259d1afd70
+	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d
 )
 
-require (
-	github.com/BurntSushi/toml v1.1.0 // indirect
-	github.com/joshuatcasey/bundler/libdependency/common v0.0.0-20220613204514-fd6ef67a090a // indirect
-)
+require github.com/BurntSushi/toml v1.1.0 // indirect
