8f4ac7edf8ed00960694e16dcf484712a536ff67
Sophie Wigmore <swigmore@vmware.com>
2022-08-17 17:10:28 -0400

    combine retrieval and metadata generation

--- dependency/retrieval/retrieve.go
+++ dependency/retrieval/retrieve.go
@@ -5,12 +5,19 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"path/filepath"
 	"sort"
+	"time"
 
 	"github.com/Masterminds/semver/v3"
+	"github.com/anchore/packageurl-go"
+	"github.com/go-enry/go-license-detector/v4/licensedb"
+	"github.com/go-enry/go-license-detector/v4/licensedb/filer"
 	"github.com/joshuatcasey/bundler/libdependency/common"
 	"github.com/paketo-buildpacks/packit/v2/cargo"
+	"github.com/paketo-buildpacks/packit/v2/vacation"
 	"golang.org/x/exp/slices"
 )
 
@@ -40,20 +47,23 @@ func main() {
 		panic("No versions found")
 	}
 
-	retrievalOutput := common.RetrievalOutput{
-		Versions: versionsFilteredByPatches,
-		ID:       id,
-		Name:     "Bundler",
+	fmt.Printf("generating metadata for %v", versionsFilteredByPatches)
+	allDependencies := []cargo.ConfigMetadataDependency{}
+	for _, version := range versionsFilteredByPatches {
+		dependencyVersion := getDependencyVersion(version)
+		dependencyVersion.ID = "bundler"
+		dependencyVersion.Name = "Bundler"
+		dependencyVersion.Stacks = []string{"bionic", "jammy"}
+		allDependencies = append(allDependencies, dependencyVersion)
 	}
 
-	bytes, err := json.Marshal(retrievalOutput)
+	bytes, err := json.Marshal(allDependencies)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("cannot marshal: %w", err))
 	}
-
 	err = os.WriteFile(output, bytes, os.ModePerm)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("cannot write to %s: %w", output, err))
 	}
 
 	fmt.Println(string(bytes))
@@ -161,4 +171,173 @@ func getBuildpackVersions(config cargo.Config) []string {
 		buildpackVersions = append(buildpackVersions, d.Version)
 	}
 	return buildpackVersions
+}
+
+func getDependencyVersion(version string) cargo.ConfigMetadataDependency {
+	bundlerReleases := getPrettyRubyGemVersions()
+
+	depURL := fmt.Sprintf("https://rubygems.org/downloads/bundler-%s.gem", version)
+	for _, release := range bundlerReleases {
+		if release.Version.String() == version {
+			return cargo.ConfigMetadataDependency{
+				Version:         version,
+				Source:          depURL,
+				SourceSHA256:    release.SHA,
+				DeprecationDate: nil,
+				CPE:             fmt.Sprintf("cpe:2.3:a:bundler:bundler:%s:*:*:*:*:ruby:*:*", version),
+				PURL:            generatePURL("bundler", version, release.SHA, depURL),
+				Licenses:        lookupLicenses(depURL),
+			}
+		}
+	}
+
+	return cargo.ConfigMetadataDependency{}
+}
+
+type RawBundlerRelease struct {
+	Version string `json:"number"`
+	Date    string `json:"created_at"`
+	SHA     string `json:"sha"`
+}
+
+type PrettyBundlerRelease struct {
+	Version     *semver.Version
+	ReleaseDate time.Time
+	SHA         string
+}
+
+func getPrettyRubyGemVersions() []PrettyBundlerRelease {
+	resp, err := http.Get("https://rubygems.org/api/v1/versions/bundler.json")
+	if err != nil {
+		panic("could not get release json")
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic("coudln't read response body")
+	}
+
+	var bundlerReleases []RawBundlerRelease
+	err = json.Unmarshal(body, &bundlerReleases)
+	if err != nil {
+		panic("could not unmarshal response")
+	}
+
+	var rubyGemVersions []PrettyBundlerRelease
+	for _, br := range bundlerReleases {
+		var pretty PrettyBundlerRelease
+		pretty.Version, err = semver.NewVersion(br.Version)
+		if err != nil {
+			continue
+		}
+
+		pretty.ReleaseDate, err = time.Parse(time.RFC3339Nano, br.Date)
+		if err != nil {
+			continue
+		}
+
+		pretty.SHA = br.SHA
+
+		rubyGemVersions = append(rubyGemVersions, pretty)
+	}
+
+	return rubyGemVersions
+}
+
+func lookupLicenses(sourceURL string) []interface{} {
+	// getting the dependency artifact from sourceURL
+	resp, err := http.Get(sourceURL)
+	if err != nil {
+		panic(fmt.Errorf("failed to query url: %w", err))
+	}
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("failed to query url %s with: status code %d", sourceURL, resp.StatusCode))
+	}
+
+	// decompressing the dependency artifact
+	tempDir, err := os.MkdirTemp("", "destination")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	err = bundlerDecompress(resp.Body, tempDir)
+	if err != nil {
+		panic(err)
+	}
+
+	// scanning artifact for license file
+	filer, err := filer.FromDirectory(tempDir)
+	if err != nil {
+		panic(fmt.Errorf("failed to setup a licensedb filer: %w", err))
+	}
+
+	licenses, err := licensedb.Detect(filer)
+	// if no licenses are found, just return an empty slice.
+	if err != nil {
+		if err.Error() != "no license file was found" {
+			panic(fmt.Errorf("failed to detect licenses: %w", err))
+		}
+		return []interface{}{}
+	}
+
+	// Only return the license IDs, in alphabetical order
+	var licenseIDs []string
+	for key := range licenses {
+		licenseIDs = append(licenseIDs, key)
+	}
+	sort.Strings(licenseIDs)
+
+	var licenseIDsAsInterface []interface{}
+	for _, licenseID := range licenseIDs {
+		licenseIDsAsInterface = append(licenseIDsAsInterface, licenseID)
+	}
+
+	return licenseIDsAsInterface
+}
+
+// The bundler dependency comes as a .gem file (tar.gz mime type) with a
+// data.tar.gz file inside that contains the license.
+func bundlerDecompress(artifact io.Reader, destination string) error {
+	archive := vacation.NewArchive(artifact)
+	err := archive.Decompress(destination)
+	if err != nil {
+		return fmt.Errorf("failed to decompress source file: %w", err)
+	}
+
+	innerArtifact, _ := os.Open(filepath.Join(destination, "data.tar.gz"))
+	innerArchive := vacation.NewArchive(innerArtifact)
+	err = innerArchive.Decompress(destination)
+	if err != nil {
+		return fmt.Errorf("failed to decompress inner source file: %w", err)
+	}
+
+	return nil
+}
+
+func generatePURL(name, version, sourceSHA, source string) string {
+	purl := packageurl.NewPackageURL(
+		packageurl.TypeGeneric,
+		"",
+		name,
+		version,
+		packageurl.QualifiersFromMap(map[string]string{
+			"checksum":     sourceSHA,
+			"download_url": source,
+		}),
+		"",
+	)
+
+	// Unescape the path to remove the added `%2F` and other encodings added to
+	// the URL by packageurl-go
+	// If the unescaping fails, we should still return the path URL with the
+	// encodings, packageurl-go has examples with both the encodings and without,
+	// we prefer to avoid the encodings when we can for convenience.
+	purlString, err := url.PathUnescape(purl.ToString())
+	if err != nil {
+		return purl.ToString()
+	}
+
+	return purlString
 }
--- dependency/test/test.sh
+++ dependency/test/test.sh
@@ -6,17 +6,17 @@ set -o pipefail
 readonly PROGDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function hasParams() {
-  local tarball_name version
-  tarball_name="${1}"
+  local tarballPath version
+  tarballPath="${1}"
   version="${2}"
 
-  if [[ -z "${tarball_name}" ]]; then
-    echo " ⛔ specify tarball_name as the first parameter"
+  if [[ -z "${tarballPath}" ]]; then
+    echo " ⛔ specify tarballPath as the first parameter"
     exit 1
   fi
 
   if [[ -z "${version}" ]]; then
-    echo " ⛔ specify tarball_name as the first parameter"
+    echo " ⛔ specify tarballPath as the first parameter"
     exit 1
   fi
 }
@@ -24,16 +24,16 @@ function hasParams() {
 function itExists() {
   echo -n "tarball exists"
 
-  local tarball_name
-  tarball_name="${1}"
+  local tarballPath
+  tarballPath="${1}"
 
-  if [[ -z "${tarball_name}" ]]; then
-    echo " ⛔ specify tarball_name as the first parameter"
+  if [[ -z "${tarballPath}" ]]; then
+    echo " ⛔ specify tarballPath as the first parameter"
     exit 1
   fi
 
-  if [[ ! -f "${tarball_name}" ]]; then
-    echo " ⛔ ${tarball_name} does not exist"
+  if [[ ! -f "${tarballPath}" ]]; then
+    echo " ⛔ ${tarballPath} does not exist"
     exit 1
   fi
 
@@ -43,19 +43,19 @@ function itExists() {
 function itHasTheRightShebang() {
   echo -n "bin/* files have shebang of '#!/usr/bin/env ruby'"
 
-  local tarball_name
-  tarball_name="${1}"
+  local tarballPath
+  tarballPath="${1}"
 
   local shebang
 
-  shebang=$(tar -O -xf "${tarball_name}" ./bin/bundle | head -n1)
+  shebang=$(tar -O -xf "${tarballPath}" ./bin/bundle | head -n1)
   if [[ "${shebang}" != "#!/usr/bin/env ruby" ]]; then
     echo " ⛔ bin/bundle must have shebang of '#!/usr/bin/env ruby'"
     echo "shebang=${shebang}"
     exit 1
   fi
 
-  shebang=$(tar -O -xf "${tarball_name}" ./bin/bundler | head -n1)
+  shebang=$(tar -O -xf "${tarballPath}" ./bin/bundler | head -n1)
   if [[ "${shebang}" != "#!/usr/bin/env ruby" ]]; then
     echo " ⛔ bin/bundler must have shebang of '#!/usr/bin/env ruby'"
     echo "shebang=${shebang}"
@@ -68,15 +68,15 @@ function itHasTheRightShebang() {
 function itHasTheRightVersion() {
   echo -n "'bin/bundle -v' prints the right version"
 
-  local tarball_name version full_tarball_name
-  tarball_name="${1}"
+  local tarballPath version full_tarballPath
+  tarballPath="${1}"
   version="${2}"
 
-  full_tarball_name=$(realpath "${tarball_name}")
+  full_tarballPath=$(realpath "${tarballPath}")
   temp_dir="$(mktemp -d)"
 
   pushd "${temp_dir}" > /dev/null || exit 1
-    tar xzf "${full_tarball_name}"
+    tar xzf "${full_tarballPath}"
 
     local output status
 
